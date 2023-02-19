565f0785d38cc311d2b12c52e9446e9a35e1435e
Kosynskyi <kosynskyi.vova@gmail.com>
2023-02-19 16:55:06 +0200

    save task

--- codewars.js
+++ codewars.js
@@ -1802,3 +1802,13 @@
 // console.log(accum('RqaEzty')); // -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 // console.log(accum('cwAt')); // -> "C-Ww-Aaa-Tttt"
 // -------------------------------------------------------------------------------------
+// const domainName = url =>
+//   url
+//     .replace(/^https?:\/\//, '')
+//     .replace(/^www\./, '')
+//     .replace(/\..*/, '');
+
+// console.log(domainName('http://google.com')); //, "google");
+// console.log(domainName('https://youtube.com')); //, "youtube");
+// console.log(domainName('www.xakep.ru')); //, "xakep");
+// -------------------------------------------------------------------------------------
