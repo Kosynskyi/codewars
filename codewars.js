aab31a1923e2672e6d85024da04fc76ae69b75e6
Kosynskyi <kosynskyi.vova@gmail.com>
2023-02-22 20:24:56 +0200

    save few tasks

--- codewars.js
+++ codewars.js
@@ -1835,3 +1835,16 @@
 // const getAge = inputString => Number(inputString[0]);
 // console.log(getAge('4 years old')); //, 4
 // -------------------------------------------------------------------------------------
+// const nbDig = (n, d) => {
+//   let res = 0;
+//   for (let i = 0; i <= n; i += 1) {
+//     const square = (i * i + '').split('');
+//     square.forEach(s => (s == d ? (res += 1) : null));
+//   }
+//   return res;
+// };
+// console.log(nbDig(5750, 0)); //,  4700, "n = 5750, d = 0");
+// console.log(nbDig(11011, 2)); //,  9481, "n = 11011, d = 2");
+// console.log(nbDig(12224, 8)); //,  7733, "n = 12224, d = 8");
+// console.log(nbDig(11549, 1)); //, 11905, "n = 11549, d = 1");
+// -------------------------------------------------------------------------------------
